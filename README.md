867306b3d3fa2ff3c85f5bf984794d5d777a7af0
charlie.nguyen <charlien@gmail.com>
2022-05-17 23:18:34 -0700

    - created some more endpoints for POST and GET/PATH - updated README.md with more examples to test

--- README.md
+++ README.md
@@ -5,4 +5,11 @@ docker compose build --no-cache
 docker compose up
 
 # To test echo server
-curl --get --data-urlencode "msg=echo message" 'localhost:8888/echo'
+## Test GET endpoint
+curl --get --data-urlencode "msg=echo message" 'localhost:8888/api/echo'
+
+## Test POST endpoint
+curl -X POST localhost:8888/api/echo -d msg="testing a post message" 
+
+## Test GET PATH endpoint
+curl localhost:8888/api/echo/this_is_a_test
--- src/main/java/com/webapp/foobar/DockerEchoController.java
+++ src/main/java/com/webapp/foobar/DockerEchoController.java
@@ -3,15 +3,39 @@ package com.webapp.foobar;
 import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DockerEchoController {
-    @GetMapping("/echo")
+    /*
+    * Endpoint for testing GET /echo with argument
+    */
+    @GetMapping("/api/echo")
     @ResponseBody
     public String getMessage(@RequestParam("msg") Optional<String> message) {
+        return message.orElse("no message sent!");
+    }
+
+    /*
+    * Endpoint for testing GET /echo/message with url encoded message
+    */
+    @GetMapping("/api/echo/{message}")
+    @ResponseBody
+    public String getMessagePath(@PathVariable Optional<String> message) {
+        return message.orElse("no message sent!");
+    }
+
+    
+    /*
+    * Endpoint for testing POST /echo with argument
+    */
+    @PostMapping("/api/echo")
+    @ResponseBody
+    public String postMessage(@RequestParam("msg") Optional<String> message) {
         return message.orElse("no message sent!");
     }
 }
